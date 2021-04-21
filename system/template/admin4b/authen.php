8de52517376db851971ac2337a484dfd3d5253fd
kinoppol <kinoppol@hotmail.com>
2021-04-21 18:40:23 +0700

    Update authen.php

--- system/template/admin4b/authen.php
+++ system/template/admin4b/authen.php
@@ -22,5 +22,27 @@ print $content;
 <?php
 print $systemFoot;
 ?>
+<div id="fb-root"></div>
+      <script>
+        window.fbAsyncInit = function() {
+          FB.init({
+            xfbml            : true,
+            version          : 'v10.0'
+          });
+        };
 
+        (function(d, s, id) {
+          var js, fjs = d.getElementsByTagName(s)[0];
+          if (d.getElementById(id)) return;
+          js = d.createElement(s); js.id = id;
+          js.src = 'https://connect.facebook.net/th_TH/sdk/xfbml.customerchat.js';
+          fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));
+      </script>
+
+      <!-- Your ปลั๊กอินแชท code -->
+      <div class="fb-customerchat"
+        attribution="page_inbox"
+        page_id="117869869613884">
+      </div>
 </html>
